b6c0c7c8b930f1a4315d7021ce35c7451dbd57c9
nitria <ntriantafillidis@yahoo.gr>
2022-02-20 17:06:17 +0200

    website performance

--- includes/main.php
+++ includes/main.php
@@ -1,29 +1,3 @@
-<?php
-$dom = new DOMDocument();
-
-$dom->loadHTMLFile('main.php');
-$img = $dom->getElementsByTagName('img');
-foreach($img as $item){
-    $item->getAttribute('src');
-}
-
-$support = false;
-if(isset($_SERVER['HTTP_ACCEPT'])){
-	$accept = $_SERVER['HTTP_ACCEPT'];
-	$pos = stripos($accept, 'image/webp');
-	if($pos === false){
-		$support = false;
-	}else{
-		$support = true;
-	}
-}
-if($support){
-    $img = getElementsByTagName();
-	echo $img;
-}
-?>
-
-
 <div class="main">
     <div class="social-icons">
         <ul>
